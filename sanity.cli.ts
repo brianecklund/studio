f27570a943bdb8cd09a2314b3f6fe230ad0cafb2
Brian Ecklund <brianecklund@gmail.com>
2025-06-18 17:56:56 +0000

    im getting this error: Error: Datasets can only contain lowercase charac

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,12 +2,12 @@
 import {defineCliConfig} from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET // Ensure this value in .env is valid: lowercase, numbers, underscores, dashes.
 
 export default defineCliConfig({
   api: {
     projectId: projectId || 'YOUR_PROJECT_ID', // Fallback, update .env
-    dataset: dataset || 'production', // Fallback, update .env
+    dataset: dataset || 'production', // Fallback, update .env. 'production' is a valid default.
   },
   // If you embedded your studio at a different path, update here
   // project: {
